266e6adc1e03ec0b1488982fcfff1902fc77c089
soryeongk <soryeongk.kr@gmail.com>
2021-10-11 16:13:09 +0900

    feat: submit input form 스타일 수정

---
+++ seminar_1st/todoList/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const routineInput = document.querySelector(".submit-form__input");
+const addBtn = document.querySelector(".submit-form__btn");
+const items = document.querySelector(".routine__item");
+
+// addBtn.addEventListener("click", () => {
+//   const routine = `<li class="routine__item --done">
+//         <div class="routine__btn"></div>
+//         <span class="routine__text">${routineInput.value}</span>
+//     </li>`;
+//   document.querySelector(".routine__items").innerHTML = routine;
+// });
