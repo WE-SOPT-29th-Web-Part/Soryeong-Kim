ed949a8a65c27ac02e70c24eed55b2ad6abc64d1
soryeongk <soryeongk.kr@gmail.com>
2021-10-25 05:46:05 +0900

    :tada:feat: D+day 기능 추가

--- calcdday/src/components/main/DateResult.jsx
+++ calcdday/src/components/main/DateResult.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-const DateResult = () => {
+const DateResult = ({ year, month, date }) => {
+  const [dayCount, setDayCount] = useState(0);
+  const [resultDate, setResultDate] = useState(`${year}년 ${month}월 ${date}일`);
+
+  const handleChange = e => {
+    const inputValue = e.target.value;
+    setDayCount(inputValue);
+    returnResult(inputValue);
+  };
+
+  const returnResult = value => {
+    const newDate = new Date();
+    newDate.setFullYear(Number(year));
+    newDate.setMonth(Number(month) - 1);
+    newDate.setDate(Number(date) + Number(value));
+
+    const result = `${newDate.getFullYear()}년 ${newDate.getMonth() + 1}월 ${newDate.getDate()}일`;
+
+    setResultDate(result);
+  };
+
+  useEffect(() => {
+    returnResult(dayCount);
+  }, [year, month, date]);
+
   return (
     <Wrapper>
       <ColWrapper>
-        <Input />
+        <Input type="number" value={dayCount} onChange={handleChange} />
         일째 되는 날은?
-        <Result>yyyy년 mm월 dd일</Result>
+        <Result>{resultDate}</Result>
       </ColWrapper>
       <ColWrapper>
         D-
         <Input />
         는?
-        <Result>yyyy년 mm월 dd일</Result>
+        <Result>
+          {year}년 {month}월 {date}일
+        </Result>
       </ColWrapper>
     </Wrapper>
   );
