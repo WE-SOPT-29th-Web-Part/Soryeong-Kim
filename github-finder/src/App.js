54be33ef400d33b1c3aed120948e99176b894511
soryeongk <soryeongk.kr@gmail.com>
2021-10-27 21:52:04 +0900

    :construction: init: 작업 세팅

---
+++ github-finder/src/App.js
@@ -0,0 +1,12 @@
+import { Header, MainWrapper, Footer } from "./components";
+function App() {
+  return (
+    <>
+      <Header />
+      <MainWrapper />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ github-finder/src/styles/global-style.js
@@ -0,0 +1,63 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+  }
+
+  @font-face {
+    font-family: "NotoSansKR";
+    font-weight: normal;
+    font-style: normal;
+    src: url("/fonts/NotoSansKR-Regular.otf") format("opentype");
+  }
+
+  @font-face {
+    font-family: "Helvetica";
+    font-weight: normal;
+    font-style: normal;
+    src: url("/fonts/Helvetica.ttf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "HelveticaBlack";
+    font-weight: normal;
+    font-style: normal;
+    src: url("/fonts/HelveticaBlack.ttf") format("truetype");
+  }
+
+  html {
+    color: #3E3E3E;
+    font-family: NotoSansKR;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  input, button {
+    outline: none; 
+    border: none;
+    background-color: transparent;                                                                                                                                                                        
+  }
+
+  input {
+    border: 0.1rem solid ${({ theme }) => theme.colors.gray3};
+  }
+
+  body {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #root {
+  }
+`;
+
+export default GlobalStyle;
