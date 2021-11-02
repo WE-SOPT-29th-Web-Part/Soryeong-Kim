710348e0339bc66d87d20c6aa0590c6e079aaa77
soryeongk <soryeongk.kr@gmail.com>
2021-11-03 03:38:05 +0900

    :house::bug: build-fix: 빌드 에러 고치는 중

--- calcdday/src/components/main/Result.jsx
+++ calcdday/src/components/main/Result.jsx
@@ -1,36 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect, useCallback } from "react";
+import styled from "styled-components";
 
 const Result = ({ sign, year, month, date }) => {
   const [dayCount, setDayCount] = useState(0);
-  const [resultDate, setResultDate] = useState(`${year}년 ${month}월 ${date}일`);
+  const [resultDate, setResultDate] = useState(
+    `${year}년 ${month}월 ${date}일`
+  );
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     const inputValue = e.target.value;
     if (inputValue < 0) return;
     setDayCount(inputValue);
     returnResult(inputValue);
   };
 
-  const returnResult = value => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const returnResult = useCallback((value) => {
     const newDate = new Date();
     newDate.setFullYear(Number(year));
     newDate.setMonth(Number(month) - 1);
-    if (sign === '+') newDate.setDate(Number(date) + Number(value));
+    if (sign === "+") newDate.setDate(Number(date) + Number(value));
     else newDate.setDate(Number(date) - Number(value));
 
-    const result = `${newDate.getFullYear()}년 ${newDate.getMonth() + 1}월 ${newDate.getDate()}일`;
+    const result = `${newDate.getFullYear()}년 ${
+      newDate.getMonth() + 1
+    }월 ${newDate.getDate()}일`;
 
     setResultDate(result);
-  };
+  });
 
   useEffect(() => {
     returnResult(dayCount);
-  }, [year, month, date]);
+  }, [year, month, date, dayCount, returnResult]);
 
   return (
     <Wrapper>
-      {sign === '+' ? (
+      {sign === "+" ? (
         <InputWrapper>
           <Input type="number" value={dayCount} onChange={handleChange} />
           일째 되는 날은?
