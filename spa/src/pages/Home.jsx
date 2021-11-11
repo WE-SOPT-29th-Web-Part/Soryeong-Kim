28e708dea566aca514610cd17ae126d75b04cbae
soryeongk <soryeongk.kr@gmail.com>
2021-11-11 16:42:03 +0900

    :truck: clean: del unused files

--- spa/src/pages/Home.jsx
+++ spa/src/pages/Home.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ArticleContainer, Header, Profile, HomeNav } from "../components";
+import axios from "axios";
+
+import { client } from "../libs/api";
 
 const Home = () => {
+  const getArticleData = async () => {
+    const { data } = await client.get();
+    console.log(`daata`, data);
+  };
+
+  useEffect(() => {
+    getArticleData();
+  }, []);
+
   return (
     <>
-      <Header />
+      app
+      {/* <Header />
       <Profile />
       <HomeNav />
-      <ArticleContainer />
+      <ArticleContainer /> */}
     </>
   );
 };
